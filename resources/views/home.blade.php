217482ce4b674dcb4f48fc3d13bd4350a0b5dd08
Brian Ferreira <brianferreira15@hotmail.com>
2021-03-09 18:06:12 -0300

    Ajustes nas views

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,11 @@
 @extends('layouts.app')
-
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
-                </div>
-            </div>
-        </div>
-    </div>
+    <h1>{{ config('app.name') }}</h1>
+    <hr>
+    <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat facere aspernatur voluptatem eius eveniet sunt
+        earum modi repellat eos quam at, in ab quo voluptas iure, dolorem quod, illo beatae?
+    </p>
 </div>
 @endsection
