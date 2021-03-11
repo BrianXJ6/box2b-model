0ba18820d089df3ae176e35a500011d4af0d4c2d
Brian Ferreira <brianferreira15@hotmail.com>
2021-03-11 12:53:14 -0300

    Aplicando retornos de error handler ao nosso padrão de respostas

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -3,6 +3,8 @@
 namespace App\Exceptions;
 
 use Throwable;
+use Illuminate\Support\Arr;
+use Illuminate\Validation\ValidationException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 
 class Handler extends ExceptionHandler {
@@ -10,9 +12,27 @@ class Handler extends ExceptionHandler {
     protected $dontReport = [];
     protected $dontFlash  = ['current_password', 'password', 'password_confirmation'];
 
-    public function register() {
-        $this->reportable(function (Throwable $e) {
-            //
-        });
+    protected function invalidJson($request, ValidationException $exception) {
+        return response()->json([
+            'status'  => 'error',
+            'message' => $exception->getMessage(),
+            'errors'  => $exception->errors(),
+        ], $exception->status);
+    }
+
+    protected function convertExceptionToArray(Throwable $e) {
+        return config('app.debug') ? [
+            'status'    => 'error',
+            'message'   => $e->getMessage(),
+            'exception' => get_class($e),
+            'file'      => $e->getFile(),
+            'line'      => $e->getLine(),
+            'trace'     => collect($e->getTrace())->map(function ($trace) {
+                return Arr::except($trace, ['args']);
+            })->all(),
+        ] : [
+            'status' => 'error',
+            'message' => $this->isHttpException($e) ? $e->getMessage() : 'Server Error',
+        ];
     }
 }
